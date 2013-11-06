5c0c4732009f055295f8d79339cc7b7397da9434
Banksy <xbanksyxhf@gmail.com>
2013-11-06 09:15:57 -0500

    ExtensionMethods added. Forgot to add them.

---
+++ Paragon Database/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Paragon_Database
+{
+    public static class ExtensionMethods
+    {
+        public static string Hash<T>(this string input, string salt) where T : HashAlgorithm, new()
+        {
+            using (T hash = new T())
+            {
+                byte[] data = hash.ComputeHash(Encoding.UTF8.GetBytes(input + salt));
+
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in data)
+                    builder.Append(b.ToString("x2"));
+
+                return builder.ToString();
+            }
+        }
+    }
+}
